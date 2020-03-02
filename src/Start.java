a626f53eb3cfbf2fe078027405647e5ba7530d86
realsteele870 <30958037+realsteele870@users.noreply.github.com>
2020-03-02 17:15:21 -0600

    Added button functionality

--- src/Start.java
+++ src/Start.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Start extends Application{
+public class Start extends Application {
 	private Stage loginPage;
 
 	public static void main(String[] args) {
@@ -15,17 +15,30 @@ public class Start extends Application{
 		launch(args);
 
 	}
-	
+
 	public void start(Stage loginStage) throws IOException, SQLException {
-		
+
 		Parent root = FXMLLoader.load(getClass().getResource("Login_FINAL.fxml"));
 		Scene scene = new Scene(root);
 		loginPage = loginStage;
 		loginPage.setTitle("Login");
 		loginPage.setMaximized(true);
 		loginPage.setScene(scene);
-		loginPage.show();		
+		loginPage.show();
+
 	}
-	
-	
+
+	public void handleLogin() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Home_Final.fxml"));
+		Scene scene = new Scene(root);
+		Stage homePage;
+		homePage = new Stage();
+		homePage.setTitle("Home");
+		homePage.setMaximized(true);
+		homePage.setScene(scene);
+		homePage.showAndWait();
+		// loginPage.close();
+
+	}
+
 }
