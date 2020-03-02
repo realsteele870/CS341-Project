f40c8ab2945d4932732d85ad57b66d4bba78e46b
l_sin <l_sin@DESKTOP-EFD6T47>
2020-03-02 17:51:38 -0600

    Added login function

--- src/Start.java
+++ src/Start.java
@@ -2,13 +2,18 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Start extends Application {
 	private Stage loginPage;
+	@FXML private Button loginBtn;
 
 	public static void main(String[] args) {
 		Database db = new Database();
@@ -28,7 +33,7 @@ public class Start extends Application {
 
 	}
 
-	public void handleLogin() throws IOException {
+	/*public void handleLogin() throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("Home_Final.fxml"));
 		Scene scene = new Scene(root);
 		Stage homePage;
@@ -39,6 +44,17 @@ public class Start extends Application {
 		homePage.showAndWait();
 		// loginPage.close();
 
+	}*/
+	@FXML
+	public void login(ActionEvent event) throws IOException, SQLException {
+	
+		Parent menuParent = FXMLLoader.load(getClass().getResource("Home_Final.fxml"));
+		Scene scene = loginBtn.getScene(); // use button to get current scene
+		Scene scene2 = new Scene(menuParent,scene.getWidth(),scene.getHeight()); // create new scene with last scenes dimensions
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene2);
+		window.setTitle("Home");
+		window.show();
 	}
 
 }
