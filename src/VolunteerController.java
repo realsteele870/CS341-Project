03665bb1aa40a38fe5ca9e98a594c071c1cf664b
NoahGust <47370488+NoahGust@users.noreply.github.com>
2020-03-03 17:25:17 -0600

    fuck2.0

--- src/VolunteerController.java
+++ src/VolunteerController.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
 
 public class VolunteerController implements Initializable {
 	@FXML
