607f13b2223d3afa1ef01379c12b21432633ab3a
benlamasney <benlamasney@gmail.com>
2020-03-02 18:54:36 -0600

    Small sstuff
    small stuff

--- src/Event.java
+++ src/Event.java
@@ -1,26 +1,25 @@
-import java.sql.Date;
-import java.sql.Time;
+
+import java.text.SimpleDateFormat;
 
 
 public class Event {
 	String name;
 	String description;
-	Date date;
+	SimpleDateFormat date;
 	String address;
-	Time time_start;
-	Time time_end;
+	int time_start;
+	int time_end;
 	int vol_needed;
 	int vol_filled;
-	@SuppressWarnings("deprecation")
-	public Event(String name, String description, String date, String address, int time_S, int time_E, int vol_needed, int vol_filled ) {
+	public Event(String name, String description, String date, String address, int time_S, int time_E, int vol_needed ) {
 		this.name = name;
 		this.description = description;
 		//this.date = new Date();
 		this.address = address;
-		this.time_start = new Time(time_S,0,0);
-		this.time_end   = new Time(time_E,0,0);
+		this.time_start = time_S;
+		this.time_end   = time_E;
 		this.vol_needed = vol_needed;
-		this.vol_filled = vol_filled;
+		this.vol_filled = 0;
 	}
 	String getName() {
 		return this.name;
@@ -38,7 +37,7 @@ public class Event {
 		return this.date.toString();
 	}
 	void setDate(String date) {
-		//this.date = new Date();
+		//this.date = date;
 	}
 	String getAddress() {
 		return this.address;
@@ -46,11 +45,23 @@ public class Event {
 	void setAddress(String address) {
 		this.address = address;
 	}
-	String getTimeStart() {
-		return this.time_start.toString();
+	int getTimeStart() {
+		return this.time_start;
 	}
-	void setStartTime(int time) {
-		
+	void setTimeStart(int time) {
+		this.time_start = time;
+	}
+	int getTimeEnd() {
+		return this.time_start;
+	}
+	void setTimeEnd(int time) {
+		this.time_start = time;
+	}
+	int getVolNeeded() {
+		return this.vol_needed;
+	}
+	void addVol() {
+		this.vol_filled++;
 	}
 	
 }
