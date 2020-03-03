86768931de0a30bfa91a7a17059ca956437e2dfe
NoahGust <47370488+NoahGust@users.noreply.github.com>
2020-03-03 17:17:55 -0600

    shit shit shit

---
+++ src/VolunteerController.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class VolunteerController implements Initializable {
+	@FXML
+	private Button volunteer;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+	}
+
+	@FXML
+	private void volunteer(ActionEvent event) throws IOException {
+		// get associated event from database and increment volunteers filled
+		
+	}
+}
