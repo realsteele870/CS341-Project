79836cba600c23af6a8ea1b8ec06d3f2c019dd34
benlamasney <benlamasney@gmail.com>
2020-04-15 10:56:52 -0500

    database update functionality
    I have just started to implement the functionality for the database update feature. With this feature, the database will be updated through Git on a trigger-based(still have to figure out exactly when) schedule

--- src/Database.java
+++ src/Database.java
@@ -1,15 +1,24 @@
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import org.sqlite.SQLiteDataSource;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
 
 public class Database {
 
 	public Connection connection;
-
+	//Database Git update-objects
+	public Git git;
+	public Repository liveRepository;
+	private CredentialsProvider cp;
+	String databaseLocation = "nasa.db";
 	public Database() {
         SQLiteDataSource ds = null;
 
@@ -27,10 +36,21 @@ public class Database {
             e.printStackTrace();
             System.exit( 0 );
         }
+        //connection to local repository
+        try {
+			liveRepository = new FileRepositoryBuilder()
+					.findGitDir()
+					.build();
+		} catch (IOException e) {
+			System.err.print("Unable to locate local repository");
+			e.printStackTrace();
+		}
 	}
 	
 	public void connect() throws SQLException {
 		connection = DriverManager.getConnection("jdbc:sqlite:nasa.db");
+		git = new Git(liveRepository);
+		
 	}
 	
 	public void disconnect() throws SQLException {
