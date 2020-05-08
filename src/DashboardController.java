6baa1ae4b5732ba9099d7f60dc488afa532bcdb3
realsteele870 <30958037+realsteele870@users.noreply.github.com>
2020-05-07 22:32:58 -0500

    Dashboard

---
+++ src/DashboardController.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class DashboardController implements Initializable {
+	@FXML
+	private Button homeBtn;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@FXML
+	private void home(ActionEvent event) throws IOException {
+		Parent menuParent = FXMLLoader.load(getClass().getResource("Home_Final.fxml"));
+		Scene scene = homeBtn.getScene(); // use button to get current scene
+		Scene scene2 = new Scene(menuParent, scene.getWidth(), scene.getHeight()); // create new scene with last scenes
+																					// dimensions
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene2);
+		window.setTitle("Home");
+		window.show();
+	}
+
+}
